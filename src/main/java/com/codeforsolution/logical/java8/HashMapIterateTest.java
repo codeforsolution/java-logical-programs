72b262e7b92705f28fa9c5966e3a38eb9a849cd9
codeforsolution <codeforyoursolution@gmail.com>
2024-02-03 17:56:31 +0530

    Write a program to iterate a HashMap using Ierator.

---
+++ src/main/java/com/codeforsolution/logical/java8/HashMapIterateTest.java
@@ -0,0 +1,24 @@
+package com.codeforsolution.logical.java8;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapIterateTest {
+    public static void main(String[] args) {
+        Map<Integer,String> hm= new HashMap<>();
+        hm.put(1,"Ram");
+        hm.put(2,"Shyam");
+        hm.put(3, "vishal");
+        hm.put(4, "Anant patil");
+        hm.put(5, "satya");
+        hm.put(6,"Rakesh");
+        Iterator<Map.Entry<Integer,String>> newIterator = hm.entrySet().iterator();
+
+        while (newIterator.hasNext()){
+            Map.Entry<Integer,String>  newHm = newIterator.next();
+            System.out.println(newHm.getKey() +" = " + newHm.getValue());
+        }
+    }
+
+}

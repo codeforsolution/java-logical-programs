ee129c3df2b4afb28c84f4e582c3d04ab4505265
codeforsolution <codeforyoursolution@gmail.com>
2024-02-11 11:20:54 +0530

    Find the longest common prefix from the String array.

---
+++ src/main/java/com/codeforsolution/logical/java/FindLongestPrefix.java
@@ -0,0 +1,33 @@
+package com.codeforsolution.logical.java;
+
+/**
+ * Find the longest common prefix from the String array.
+ * Input:-  {"javaprogramming", "javadeveloper", "javaprogrammer", "javaarchitect"}
+ *
+ * Output:- java
+ */
+public class FindLongestPrefix {
+
+    public static void main(String[] args) {
+        String[] arr = {"javaprogramming", "javadeveloper", "javaprogrammer", "javaarchitect"};//{"giorgi", "gio", "gior", "giorg", "gior"};
+        System.out.println(longestCommonPrefix(arr));
+    }
+
+    public static String longestCommonPrefix(String[] arr) {
+        if(arr.length==0) return "";
+        String prefix = arr[0];
+        for(int i = 1;i<arr.length; i++){
+
+            while(arr[i].indexOf(prefix) !=0){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.equals("")){
+                    return "";
+                }
+            }
+
+        }
+        return prefix;
+    }
+
+
+}

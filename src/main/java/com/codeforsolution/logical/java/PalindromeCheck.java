4a11c5e9467104024b7f47493a1cf37975c1d784
codeforsolution <codeforyoursolution@gmail.com>
2024-07-06 23:23:39 +0530

    Write a program to check a number is Palindrome or not

---
+++ src/main/java/com/codeforsolution/logical/java/PalindromeCheck.java
@@ -0,0 +1,33 @@
+/**
+ * @Author codeforsolution
+ * Write a program to check a number is palindrome or not
+ * Date: 30-03-2024
+ **/
+package com.codeforsolution.logical.java;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        int number, reverse=0, tempNumber, remainder;
+        System.out.println("Enter a number");
+        Scanner s = new Scanner(System.in);
+        number = s.nextInt();
+        s.close();
+        tempNumber = number;
+        while (number != 0){
+           remainder = number % 10;
+           reverse = reverse * 10 + remainder;
+           number = number / 10;
+        }
+
+        if(tempNumber == reverse){
+            System.out.println("Number is palindrome");
+        } else {
+            System.out.println("Number is not palindrome");
+        }
+
+    }
+}
